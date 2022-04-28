1db915e99018efc969e92b92483583da482c13b8
zoolake <junhomoon224@gmail.com>
2022-04-28 16:12:32 +0900

    Init : [BE] 스켈레톤 코드 작성

---
+++ backend/src/main/java/finale/bloombloom/common/model/response/Result.java
@@ -0,0 +1,15 @@
+package finale.bloombloom.common.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class Result<T> {
+    private T data;
+    private int status;
+    private String message;
+}
+
