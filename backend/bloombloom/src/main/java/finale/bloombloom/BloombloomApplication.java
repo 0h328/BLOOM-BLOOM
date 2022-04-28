03feb8d963e1ec3bd0cc8ceb87ac09d4780832c4
zoolake <junhomoon224@gmail.com>
2022-04-28 14:11:48 +0900

    Init : [BE] 스켈레톤 코드

---
+++ backend/bloombloom/src/main/java/finale/bloombloom/BloombloomApplication.java
@@ -0,0 +1,13 @@
+package finale.bloombloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloombloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloombloomApplication.class, args);
+	}
+
+}
---
+++ backend/bloombloom/src/test/java/finale/bloombloom/BloombloomApplicationTests.java
@@ -0,0 +1,13 @@
+package finale.bloombloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloombloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
