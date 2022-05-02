dffd33164520a29c89427b34223fe8096178c003
dkfkqldk97 <dkfkqldk97@naver.com>
2022-05-02 16:19:05 +0900

    fix : fix 로그인 페이지

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -27,47 +27,7 @@ function Login() {
         >
           <Title />
           <FlowerImg />
-          {/* <Box
-            sx={{
-              position: "absolute",
-              top: "680px",
-              left: "70px",
-              backgroundColor: "#FEE500",
-              width: "280px",
-              height: "45px",
-              borderRadius: "5px",
-            }}
-          >
-            <Box
-              sx={{
-                position: "absolute",
-                alignItems: "center",
-                justifyItems: "center",
-                top: "8px",
-                left: "30px",
-              }}
-            >
-              <Image
-                src="/img/kakaoLogo.png"
-                alt="KakaoLogo"
-                width={30}
-                height={33}
-              ></Image>
-            </Box>
-            <Typography
-              style={{
-                position: "absolute",
-                top: "12px",
-                left: "80px",
-                fontWeight: "600",
-                fontFamily: "JuliusSansOne",
-                fontSize: "15px",
-                color: "#3A1D1D",
-              }}
-            >
-              카카오톡으로 시작하기
-            </Typography>
-          </Box> */}
+
           <Box sx={{ position: "absolute", top: "730px", left: "70px" }}>
             <KakaoBtn
               handleBtn={handleLogin}
