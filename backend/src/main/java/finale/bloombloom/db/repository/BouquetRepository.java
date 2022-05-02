672567b30f64590a646206d87696a286bcc5242c
zoolake <junhomoon224@gmail.com>
2022-05-02 15:25:56 +0900

    feat : [BE] 유저가 보유한 꽃다발 리스트 조회 기능 구현
    Authentication에서 유저 정보를 추출하는 관련 코드들이 미완성 상태여서 임시로 userSeq 변수를 생성하여 구현 완료하였습니다.

---
+++ backend/src/main/java/finale/bloombloom/db/repository/BouquetRepository.java
@@ -0,0 +1,10 @@
+package finale.bloombloom.db.repository;
+
+import finale.bloombloom.db.entity.Bouquet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BouquetRepository extends JpaRepository<Bouquet, Long> {
+    List<Bouquet> findAllByUserSeq(Long userSeq);
+}
