b877e05f799d2478e97755db9428ce8d0595be99
박건우 <hggygw77@naver.com>
2022-05-06 15:11:33 +0900

    Feat : [BE] 관리자 업장 리스트 조회

---
+++ backend/src/main/java/finale/bloombloom/api/response/StoreListResponse.java
@@ -0,0 +1,29 @@
+package finale.bloombloom.api.response;
+
+import finale.bloombloom.db.entity.Store;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreListResponse {
+    private Long storeSeq;
+    private String storeName;
+    private String storeContact;
+    private String storeAddress;
+    private String storeRegNum;
+
+    public static StoreListResponse from(Store store) {
+        return StoreListResponse.builder()
+                .storeSeq(store.getStoreSeq())
+                .storeName(store.getStoreName())
+                .storeContact(store.getStoreContact())
+                .storeAddress(store.getStoreAddress())
+                .storeRegNum(store.getStoreRegNum())
+                .build();
+    }
+}
--- backend/src/main/java/finale/bloombloom/api/service/AdminServiceImpl.java
+++ backend/src/main/java/finale/bloombloom/api/service/AdminServiceImpl.java
@@ -1,15 +1,26 @@
 package finale.bloombloom.api.service;
 
 import finale.bloombloom.api.request.AdminSaveRequest;
+import finale.bloombloom.api.response.StoreListResponse;
 import finale.bloombloom.db.entity.Store;
 import finale.bloombloom.db.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService{
     private final StoreRepository storeRepository;
+
+    @Override
+    public List<StoreListResponse> findAllStore() {
+        return storeRepository.findAll().stream()
+                .map(StoreListResponse::from)
+                .collect(Collectors.toList());
+    }
 
     @Override
     public Store saveStore(AdminSaveRequest req) {
