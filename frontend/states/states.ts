c1c76f7599e8715f50817412cc95d12e7032d1cb
0h328 <mage888@naver.com>
2022-05-10 14:50:02 +0900

    fix : [FE] 꽃다발 선택하기 페이지 수정

--- frontend/states/states.ts
+++ frontend/states/states.ts
@@ -20,9 +20,15 @@ const mainFlowerState = atom({
   default: [{ flowerSeq: 1, flowerCount: 1 }],
 });
 
+// 포장지
 const wrapState = atom({
   key: "wrapInfo",
-  default: { wrapSeq: 1, wrapImage: "/img/wrapOrange.png" },
+  default: { 
+    wrapSeq: 1, 
+    wrapImage: "/img/wrapOrange.png",
+    wrapBackImage: "/img/wrapBackOrange.png",
+    wrapFrontImage: "/img/wrapFrontOrange.png"
+   },
 })
 
 // 리본
@@ -31,6 +37,7 @@ const decoState = atom({
   default: { decoSeq: 1, decoImage: "/img/ribbonDeepPink.png" },
 })
 
+// 부속꽃
 const flowerState = atom({
   key: "flowerInfo",
   default: { flowerSeq: 1, flowerImage: "/img/flower1.png" },
