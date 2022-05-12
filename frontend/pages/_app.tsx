0a409664a1a570d7db2abec0f94dc4492fb5d11b
wjdgur778 <wjdgur778@gmail.com>
2022-05-12 10:56:10 +0900

    Feat : [FE] 카카오 링크 공유하기

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,13 +4,13 @@ import "../styles/globals.css";
 import Script from "next/script";
 declare global {
   interface Window {
-    kakao: any;
+    Kakao: any;
   }
 }
 function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Script src="https://developers.kakao.com/sdk/js/kakao.js"></Script>
+      {/* <Script src="https://developers.kakao.com/sdk/js/kakao.js"></Script> */}
       <Component {...pageProps} />
     </RecoilRoot>
   );
