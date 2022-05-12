1f074d0068e2918278ab1857e60ddc9f64531e2f
dkfkqldk97 <dkfkqldk97@naver.com>
2022-05-12 10:04:15 +0900

    fix : 이미지  수정

--- frontend/components/login/FlowerImg.tsx
+++ frontend/components/login/FlowerImg.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Box } from "@mui/material";
 import { BASE_URL } from "../apis/config";
 import Image from "next/image";
-import loginImg from "../../public/img/loginFlower2.png";
 function FlowerImg() {
   return (
     // <Box sx={{ position: "" }}>
@@ -15,13 +14,12 @@ function FlowerImg() {
         }}
       >
         <img
-          src={loginImg}
+          src="/img/loginFlower1.jpg"
           style={{
             borderRadius: "200px",
             height: "100%",
             width: "100%",
           }}
-          layout="fill"
         ></img>
       </Box>
       <Box
