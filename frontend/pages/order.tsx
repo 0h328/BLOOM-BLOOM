4faea8c485f3f6c0d8e755c5a1860dfe59016b23
dkfkqldk97 <dkfkqldk97@naver.com>
2022-05-18 21:16:25 +0900

    Fix : [FE] 빌드 오류 수정

--- frontend/pages/order.tsx
+++ frontend/pages/order.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { Box, Button } from "@mui/material";
 import StoreCardSmall from "../components/order/StoreCardSmall";
 import OrderFlower from "../components/order/OrderFlower";
@@ -7,38 +8,48 @@ import MakeButton from "../components/main/MakeButton";
 import { useRouter } from "next/router";
 
 export default function Order() {
+  const [store, setStore] = useState();
   const router = useRouter();
-  const store = JSON.parse(String(router.query.storeInfo));
+  // const store = JSON.parse(String(router.query.storeInfo));
   const bouquetSeq = router.query.bouquetSeq;
   console.log(bouquetSeq);
+  useEffect(() => {
+    setStore(JSON.parse(String(router.query.storeInfo)));
+    console.log(store);
+  }, []);
+
   return (
-    <div
+    <Box
       style={{
         width: "420px",
         height: "926px",
         margin: "auto",
       }}
     >
-      <Box sx={{ height: 30 }}></Box>
-      <Header page="order"></Header>
-      <StoreCardSmall storeInfo={store}></StoreCardSmall>
-      <OrderFlower bouquetSeq={Number(bouquetSeq)}></OrderFlower>
-      <Request></Request>
-      <Box sx={{ width: 156, mx: "auto" }}>
-        <Button
-          sx={{
-            width: 156,
-            height: 36,
-            backgroundColor: "#FFE0E0",
-            color: "#000000",
-            fontFamily: "OneMobileLight",
-            fontWeight: "bold",
-          }}
-        >
-          {" "}
-          주문의뢰하기{" "}
-        </Button>
-      </Box>
-    </div>
+      {store !== undefined ? (
+        <>
+          <Box sx={{ height: 30 }}></Box>
+          <Header page="order"></Header>
+          <StoreCardSmall storeInfo={store}></StoreCardSmall>
+          <OrderFlower bouquetSeq={Number(bouquetSeq)}></OrderFlower>
+          <Request></Request>
+          <Box sx={{ width: 156, mx: "auto" }}>
+            <Button
+              sx={{
+                width: 156,
+                height: 36,
+                backgroundColor: "#FFE0E0",
+                color: "#000000",
+                fontFamily: "OneMobileLight",
+                fontWeight: "bold",
+              }}
+            >
+              {" "}
+              주문의뢰하기{" "}
+            </Button>
+          </Box>
+        </>
+      ) : null}
+    </Box>
   );
 }
