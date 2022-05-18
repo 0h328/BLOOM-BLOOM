476abb7b58d51cd2ce4d64a53c778a93b3dbae92
dkfkqldk97 <dkfkqldk97@naver.com>
2022-05-19 02:27:30 +0900

    Design : [FE] fix 레이아웃

--- frontend/pages/main.tsx
+++ frontend/pages/main.tsx
@@ -57,7 +57,7 @@ function Main() {
       <Box
         sx={{
           mx: "auto",
-          width: 420,
+          width: 430,
           position: "relative",
           backgroundColor: "#FFE0E0",
           height: "100vh",
