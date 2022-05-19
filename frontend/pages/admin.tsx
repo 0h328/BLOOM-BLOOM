a910eeffb9060274a30df7e1067d1a198b335bc2
0h328 <mage888@naver.com>
2022-05-20 06:22:37 +0900

    fix : [FE] 빌드 에러 해결

--- frontend/pages/admin.tsx
+++ frontend/pages/admin.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../components/admin/Navbar';
 import Board from '../components/admin/Board';
 import Footer from '../components/admin/Footer';
-import RegisterModal from '../components/admin/RegisterModal';
 import { Box } from '@mui/material';
 
 function Admin() {
@@ -10,7 +9,7 @@ function Admin() {
   return (
     <Box>
       <Navbar/>      
-      <Board/>  
+      <Board storeInfoList={[]}/>  
       <Footer/>
     </Box>
   )
